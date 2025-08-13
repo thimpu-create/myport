c7fa0d15042f225f20eec4516456eb85afb249ea
thimpu-create <thmtymthm@gmail.com>
2025-08-13 11:42:32 +0530

    fixed next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ['images.unsplash.com'],
+    unoptimized: true, // Disable Image Optimization for static export
   },
 }
 
